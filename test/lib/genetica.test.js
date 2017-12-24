834f9cf63551f570521b400a32dd93029307b988
Drew Morris <dhmorris@gmail.com>
2017-12-24 18:12:37 -0500

    Improving tests and passing options for chromosome values

--- test/lib/genetica.test.js
+++ test/lib/genetica.test.js
@@ -17,4 +17,67 @@ describe('Genetica', () => {
 
     expect(genes).to.be.an('object');
   });
+
+  context('validates opts', () => {
+    it('checks for race and gender', () => {
+      const result = genetica.validateOpts({});
+      
+      expect(result).to.be.an('object');
+      expect(result.race).to.be.a('string');
+      expect(result.gender).to.be.a('string');
+    });
+
+    it('throws errors for poorly malformatted chromosome values', () => {
+      try {
+        const result = genetica.validateOpts({
+          'chromosome-1': '1',
+        });
+      } catch (err) {
+        expect(err.message).to.eq('Malformatted chromosome input!');
+      }
+    });
+
+    it('throws errors for too large dice rolls', () => {
+      try {
+        const result = genetica.validateOpts({
+          'chromosome-1': '100000=1',
+        });
+      } catch (err) {
+        expect(err.message).to.eq('Chromosome value higher than possible dice roll!');
+      }
+    });
+
+    it('throws errors for bad female sex chromosome', () => {
+      try {
+        const result = genetica.validateOpts({
+          gender: 'female',
+          'chromosome-sex': 'X1=Y1',
+        });
+      } catch (err) {
+        expect(err.message).to.eq('Malformatted female sex chromosome input!');
+      }
+    });
+
+    it('throws errors for bad male sex chromosome', () => {
+      try {
+        const result = genetica.validateOpts({
+          gender: 'male',
+          'chromosome-sex': 'Y1=Y1',
+        });
+      } catch (err) {
+        expect(err.message).to.eq('Malformatted male sex chromosome input!');
+      }
+    });
+
+    it('throws errors for large sex chromosome values', () => {
+      try {
+        const result = genetica.validateOpts({
+          gender: 'male',
+          'chromosome-sex': 'X100000=Y1',
+        });
+      } catch (err) {
+        expect(err.message).to.eq('Sex chromosome value higher than possible dice roll!');
+      }
+    });
+  });
 });
